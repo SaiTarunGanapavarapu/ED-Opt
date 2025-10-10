6f181340de4b5ed80adc3d13be955e43a529fb6e
Sai Tarun Ganapavarapu <159673266+SaiTarunGanapavarapu@users.noreply.github.com>
2025-10-10 16:44:08 -0400

    Update getModelEqsRHS.m

--- Simulation/Simulation/getModelEqsRHS.m
+++ Simulation/Simulation/getModelEqsRHS.m
@@ -1,5 +1,5 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% function:    ED_MemSim_SS_DAE_RHS(...)                                  %
+% function:    getModelEqsRHS(...)                                        %
 % Description: RHS function for differential and algebraic equation       %
 %                  system from Campione et al. (2019)                     %
 %                                                                         %
@@ -89,5 +89,6 @@ function [RHS] = getModelEqsRHS(z, stateVar, diffVar, algVar, j)
     RHS(6) = dcurr - currDens*memWidth;
     
 end
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
